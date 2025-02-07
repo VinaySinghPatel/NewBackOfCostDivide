a8ce35df8d971cd8d579d6b235f19a603b504f46
Vinay Singh Patel <vinaypatel898944@gmail.com>
2025-02-08 00:13:06 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 const connectTomongo = async () => {
     try {
         await mongoose.connect('mongodb+srv://dhruvdwivedi2304:vinay@cluster0.ymzpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
          });
          console.log("We are connected to the MongoDB Server");
     } catch (error) {
-       console.log("Something error while connecting to the databse");
+       console.log("Something error while connecting to the databse Server");
     }
 }
 
